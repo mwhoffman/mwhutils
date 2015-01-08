b18a7ab290eaae4e0fce7659ae31f43ed0de783e
Matt Hoffman <mwh30@cam.ac.uk>
2015-01-08 18:22:00 +0000

    Update ABC wrapper to deal with class/static methods.

--- mwhutils/abc.py
+++ mwhutils/abc.py
@@ -9,10 +9,11 @@ from __future__ import print_function
 
 # global imports
 from abc import ABCMeta as ABCMeta_
-from abc import abstractmethod
+from abc import abstractmethod, abstractproperty
 
 # exported symbols
-__all__ = ['ABCMeta', 'abstractmethod']
+__all__ = ['ABCMeta', 'abstractmethod', 'abstractproperty',
+           'abstractclassmethod', 'abstractstaticmethod']
 
 
 class ABCMeta(ABCMeta_):
@@ -22,16 +23,55 @@ class ABCMeta(ABCMeta_):
     docstring.
     """
     def __new__(mcs, name, bases, attrs):
+        # set up the new class.
+        cls = super(ABCMeta, mcs).__new__(mcs, name, bases, attrs)
+
+        # get all the methods that are abtract in one of our parents.
         abstracts = dict(
             (attr, getattr(base, attr))
             for base in bases
             for attr in getattr(base, '__abstractmethods__', set()))
 
-        for attr, value in attrs.items():
-            implements = (attr in abstracts and
-                          not getattr(value, '__isabstractmethod__', False))
-            if implements and not getattr(value, '__doc__', False):
-                docstring = getattr(abstracts[attr], '__doc__', None)
-                setattr(value, '__doc__', docstring)
+        # loop through the methods that have an implementation in the class
+        # that we just constructed.
+        for attr in set(abstracts.keys()) - cls.__abstractmethods__:
+            parent = abstracts[attr]
+            child = getattr(cls, attr)
+            docstring = getattr(parent, '__doc__', None)
 
-        return super(ABCMeta, mcs).__new__(mcs, name, bases, attrs)
+            if docstring and not getattr(child, '__doc__', None):
+                if isinstance(child, property):
+                    setattr(cls, attr, property(child.fget, child.fset,
+                                                child.fdel, docstring))
+                elif hasattr(child, '__func__'):
+                    child.__func__.__doc__ = docstring
+                else:
+                    child.__doc__ = docstring
+
+        return cls
+
+
+class abstractclassmethod(classmethod):
+    """
+    Decorator to create an abstract class method. This is a variation on the
+    decorator introduced in Python 3. It has since been deprecated since
+    classmethod can now be used to decorate an abstractmethod.
+    """
+    __isabstractmethod__ = True
+
+    def __init__(self, function):
+        function.__isabstractmethod__ = True
+        super(abstractclassmethod, self).__init__(function)
+
+
+class abstractstaticmethod(staticmethod):
+    """
+    Decorator to create an abstract static method. This is a variation on the
+    decorator introduced in Python 3. It has since been deprecated since
+    staticmethod can now be used to decorate an abstractmethod.
+    """
+    __isabstractmethod__ = True
+
+    def __init__(self, function):
+        function.__isabstractmethod__ = True
+        super(abstractstaticmethod, self).__init__(function)
--- tests/test_abc.py
+++ tests/test_abc.py
@@ -6,7 +6,8 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
 
-from mwhutils.abc import ABCMeta, abstractmethod
+import numpy.testing as nt
+import mwhutils.abc as abc
 
 
 def test_abc():
@@ -14,25 +15,75 @@ def test_abc():
 
     class FooBase(object):
         """Base class."""
-        __metaclass__ = ABCMeta
 
-        @abstractmethod
-        def method1(self):
-            """Test."""
-            pass
+        __metaclass__ = abc.ABCMeta
 
-        def method2(self):
-            """Test."""
-            pass
+        def method(self):
+            """Method"""
+
+        @abc.abstractmethod
+        def abstract_method1(self):
+            """Method 1"""
+
+        @abc.abstractmethod
+        def abstract_method2(self):
+            """Method 2"""
+
+        @abc.abstractclassmethod
+        def abstract_classmethod(cls):
+            """Class method"""
+
+        @abc.abstractstaticmethod
+        def abstract_staticmethod():
+            """Static method"""
+
+        @abc.abstractproperty
+        def abstract_property(self):
+            """Property"""
 
     class Foo(FooBase):
         """Child class."""
-        def method1(self):
+
+        def method(self):
+            """New method"""
             pass
 
-        def method2(self):
-            pass
+        def abstract_method1(self):
+            return 1
 
-    instance = Foo()
-    assert instance.method1.__doc__ == "Test."
-    assert instance.method2.__doc__ is None
+        def abstract_method2(self):
+            """Method 3"""
+            return 2
+
+        @classmethod
+        def abstract_classmethod(cls):
+            return 3
+
+        @staticmethod
+        def abstract_staticmethod():
+            return 4
+
+        @property
+        def abstract_property(self):
+            return 5
+
+    # Make sure the abstract class can't be instantiated
+    nt.assert_raises(TypeError, FooBase)
+
+    # check the docstrings
+    assert Foo.method.__doc__ == "New method"
+    assert Foo.abstract_method1.__doc__ == "Method 1"
+    assert Foo.abstract_method2.__doc__ == "Method 3"
+    assert Foo.abstract_classmethod.__doc__ == "Class method"
+    assert Foo.abstract_staticmethod.__doc__ == "Static method"
+    assert Foo.abstract_property.__doc__ == "Property"
+
+    # Make sure the instance can be instantiated
+    foo = Foo()
+
+    # check the implementations
+    assert foo.abstract_method1() == 1
+    assert foo.abstract_method2() == 2
+    assert foo.abstract_classmethod() == 3
+    assert foo.abstract_staticmethod() == 4
+    assert foo.abstract_property == 5
