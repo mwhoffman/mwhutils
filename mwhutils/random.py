993b1c66691e6ac8ad62f2639cabe74441d51315
Matt Hoffman <mwh30@cam.ac.uk>
2015-02-24 11:29:17 +0000

    Add wishart sampler.

--- mwhutils/random.py
+++ mwhutils/random.py
@@ -8,7 +8,7 @@ from __future__ import print_function
 
 import numpy as np
 
-__all__ = ['rstate']
+__all__ = ['rstate', 'wishart']
 
 
 def rstate(rng=None):
@@ -25,3 +25,27 @@ def rstate(rng=None):
     elif isinstance(rng, int):
         return np.random.RandomState(rng)
     raise ValueError('unknown seed given to rstate')
+
+
+def wishart(Sigma, dof, rng=None):
+    """
+    Return a sample from the Wishart distribution with scale matrix `Sigma` and
+    `dof` degrees of freedom.
+    """
+    rng = rstate(rng)
+    d = Sigma.shape[0]
+    L = np.linalg.cholesky(Sigma)
+
+    if (dof <= 81+d) and (dof == round(dof)):
+        # for small enough dof just use the standard definition
+        X = np.dot(L, rng.normal(size=(d, dof)))
+
+    else:
+        # otherwise use the procedure of Smith & Hocking, where the cholesky is
+        # multiplied by A which has sqrt of chi-square variables on its
+        # diagonal and standard normal elements on the upper triangle.
+        A = np.diag(np.sqrt(rng.chisquare(dof - np.arange(0, d), size=d)))
+        A[np.tri(d, k=-1, dtype=bool)] = rng.normal(size=(d*(d-1)/2.))
+        X = np.dot(L, A)
+
+    return np.dot(X, X.T)
