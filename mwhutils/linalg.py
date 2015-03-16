1c4fe90e02f8328daa3a11ff900c349614ab47ba
Matt Hoffman <mwh30@cam.ac.uk>
2015-03-16 14:00:31 +0000

    Add more linalg methods.

--- mwhutils/linalg.py
+++ mwhutils/linalg.py
@@ -1,5 +1,5 @@
 """
-Linear algebra helper functions.
+Linear algebra helpers.
 """
 
 from __future__ import division
@@ -7,30 +7,103 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import numpy as np
-import scipy.linalg as sla
+import warnings
 
-__all__ = ['chol_update']
+from scipy.linalg import lapack
+from scipy.linalg import LinAlgError
+from numpy.lib.stride_tricks import as_strided
 
 
-def chol_update(A, B, C, a, b):
+__all__ = ['add_diagonal', 'cholesky', 'cholesky_inverse', 'cholesky_update',
+           'solve_triangular', 'solve_cholesky']
+
+
+def add_diagonal(A, d, copy=False):
+    """
+    Return the matrix `A` where its diagonal has had the vector `d` added to
+    it. If `copy` is False perform the addition in place.
+    """
+    if copy:
+        A = A.copy()
+    shape = (A.shape[0],)
+    strides = ((A.shape[0]+1)*A.itemsize,)
+    D = as_strided(A, shape, strides)
+    D += d
+    return A
+
+
+def cholesky(A, maxtries=5):
+    """
+    Compute the cholesky of A. If the matrix is singular make `maxtries`
+    additional attempts to invert it by adding small amounts to the diagonal
+    before giving up.
+    """
+    L, info = lapack.dpotrf(A, lower=1)
+    if info == 0:
+        return L
+    else:
+        d = A.diagonal()
+        if np.any(d <= 0):
+            raise LinAlgError('Matrix has non-positive diagonal elements')
+        j = d.mean() * 1e-6
+        for _ in xrange(maxtries):
+            L, info = lapack.dpotrf(add_diagonal(A, j, True), lower=1)
+            if info == 0:
+                message = 'jitter of {:s} required to compute the cholesky'
+                warnings.warn(message.format(str(j)), stacklevel=2)
+                return L
+            else:
+                j *= 10
+        raise LinAlgError('Matrix is not positive definite, even with jitter')
+
+
+def cholesky_update(L, B, C, x=None, c=None):
     """
     Update the cholesky decomposition of a growing matrix.
 
-    Let `A` denote a cholesky decomposition of some matrix and `a` the inverse
-    of `A` applied to some vector `y`. This computes the cholesky to a new
-    matrix which has additional elements `B` and the non-diagonal and `C` on
-    the diagonal block. It also computes the solution to the application of the
-    inverse where the vector has additional elements `b`.
+    This computes the lower-triangular cholesky decomposition of [A B'; B C]
+    where L, the cholesky decomposition of A, has been previously computed.
     """
-    n = A.shape[0]
-    m = C.shape[0]
+    # grow the new cholesky
+    B = solve_triangular(L, B.T).T
+    C = cholesky(C - np.dot(B, B.T))
+    L = np.r_[np.c_[L, np.zeros_like(B.T)], np.c_[B, C]]
 
-    B = sla.solve_triangular(A, B, trans=True)
-    C = sla.cholesky(C - np.dot(B.T, B))
-    c = np.dot(B.T, a)
+    if x is None:
+        return L
 
-    # grow the new cholesky and use then use this to grow the vector a.
-    A = np.r_[np.c_[A, B], np.c_[np.zeros((m, n)), C]]
-    a = np.r_[a, sla.solve_triangular(C, b-c, trans=True)]
+    else:
+        x = np.r_[x, solve_triangular(C, c - np.dot(B, x))]
+        return L, x
 
-    return A, a
+
+def cholesky_inverse(A):
+    """
+    Compute the inverse of the matrix AA' given its cholesky decomposition A.
+    """
+    X, info = lapack.dpotri(A, lower=1)
+    if info != 0:
+        raise LinAlgError('Matrix is not invertible')
+    triu = np.triu_indices_from(X, k=1)
+    X[triu] = X.T[triu]
+    return X
+
+
+def solve_triangular(A, B, trans=False):
+    """
+    Solve the system Ax=B where A is lower triangular. If `trans` is True then
+    solve the system A'x=B.
+    """
+    X, info = lapack.dtrtrs(A, B, lower=1, trans=int(trans))
+    if info != 0:
+        raise LinAlgError('Matrix is singular')
+    return X
+
+
+def solve_cholesky(A, B):
+    """
+    Solve the system Mx=B where A is the lower-triangular cholesky
+    decomposition of the matrix M.
+    """
+    X, _ = lapack.dpotrs(A, B, lower=1)
+    return X
--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -1,29 +1,85 @@
 """
-Linalg tests.
+Tests for linear algebra helpers.
 """
+
+# pylint: disable=missing-docstring
 
 from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
 
 import numpy as np
-import scipy.linalg as sla
 import numpy.testing as nt
 
-from mwhutils.linalg import chol_update
+import mwhutils.linalg as linalg
 
 
-def test_chol_update():
-    """Test the incremental cholesky decomposition."""
+def test_add_diagonal():
     A = np.random.rand(5, 5)
-    A = np.dot(A.T, A)
-    b = np.random.rand(5, 2)
+    B = A.copy()
+    C = linalg.add_diagonal(B, 1, copy=True)
+    D = linalg.add_diagonal(B, 1)
 
-    R1 = sla.cholesky(A[:3, :3])
-    x1 = sla.solve_triangular(R1, b[:3], trans=True)
-    R1, x1 = chol_update(R1, A[:3, 3:], A[3:, 3:], x1, b[3:])
+    nt.assert_equal(C, A + np.eye(5))
+    nt.assert_equal(D, A + np.eye(5))
+    assert C is not B
+    assert D is B
 
-    R2 = sla.cholesky(A)
-    x2 = sla.solve_triangular(R2, b, trans=True)
 
-    nt.assert_allclose(x1, x2)
+def test_cholesky():
+    A = np.random.rand(5, 5)
+    A = linalg.add_diagonal(np.dot(A, A.T), 1)
+    L = linalg.cholesky(A)
+    nt.assert_allclose(np.dot(L, L.T), A)
+
+    A[0, 0] = -1
+    nt.assert_raises(linalg.LinAlgError, linalg.cholesky, A)
+
+    A = np.ones((10, 10))
+    nt.assert_warns(UserWarning, linalg.cholesky, A)
+
+    A = np.ones((10, 10)) - 0.5 * np.eye(10)
+    nt.assert_raises(linalg.LinAlgError, linalg.cholesky, A)
+
+
+def test_cholesky_inverse():
+    A = np.random.rand(5, 5)
+    A = linalg.add_diagonal(np.dot(A, A.T), 1)
+    L = linalg.cholesky(A)
+    invA1 = linalg.cholesky_inverse(L)
+    invA2 = np.linalg.inv(A)
+    nt.assert_allclose(invA1, invA2)
+
+    L = np.zeros((5, 5))
+    L[:, 0] = 1
+    nt.assert_raises(linalg.LinAlgError, linalg.cholesky_inverse, L)
+
+
+def test_solve_triangular():
+    A = np.random.rand(5, 5)
+    A = linalg.add_diagonal(np.dot(A, A.T), 1)
+    L = linalg.cholesky(A)
+    B = np.random.rand(5, 2)
+
+    X1 = linalg.solve_triangular(L, B)
+    X2 = np.linalg.solve(L, B)
+    nt.assert_allclose(X1, X2)
+
+    X1 = linalg.solve_triangular(L, B, 1)
+    X2 = np.linalg.solve(L.T, B)
+    nt.assert_allclose(X1, X2)
+
+    L = np.zeros((5, 5))
+    L[:, 0] = 1
+    nt.assert_raises(linalg.LinAlgError, linalg.solve_triangular, L, B)
+
+
+def test_solve_cholesky():
+    A = np.random.rand(5, 5)
+    A = linalg.add_diagonal(np.dot(A, A.T), 1)
+    L = linalg.cholesky(A)
+    B = np.random.rand(5, 2)
+
+    X1 = linalg.solve_cholesky(L, B)
+    X2 = np.linalg.solve(A, B)
+    nt.assert_allclose(X1, X2)
