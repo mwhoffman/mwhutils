9a130a3cde08200cd47d9f2662fa2d22bbd1fe41
Matt Hoffman <mwh30@cam.ac.uk>
2015-03-31 16:16:44 +0100

    Add test for wishart rng.

--- tests/test_random.py
+++ tests/test_random.py
@@ -6,18 +6,27 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
 
+import numpy as np
 import numpy.testing as nt
-
-from mwhutils.random import rstate
+import mwhutils.random as random
 
 
 def test_rstate():
     """Test the rstate helper."""
-    rng = rstate()
-    rng = rstate(rng)
-    rng1 = rstate(1)
-    rng2 = rstate(1)
+    rng = random.rstate()
+    rng = random.rstate(rng)
+    rng1 = random.rstate(1)
+    rng2 = random.rstate(1)
 
     nt.assert_equal(rng1.randint(5), rng2.randint(5))
-    nt.assert_raises(ValueError, rstate, 'foo')
+    nt.assert_raises(ValueError, random.rstate, 'foo')
 
+
+def test_wishart():
+    Sigma1 = random.wishart(np.eye(5), 5)
+    Sigma2 = random.wishart(Sigma1, 5)
+    Sigma3 = random.wishart(Sigma1, 120)
+
+    np.linalg.cholesky(Sigma1)
+    np.linalg.cholesky(Sigma2)
+    np.linalg.cholesky(Sigma3)
