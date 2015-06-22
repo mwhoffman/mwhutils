8bc81e83d66244f2199c42aa6fd8124bc28a8909
Matt Hoffman <mwh30@cam.ac.uk>
2015-06-22 17:47:40 +0100

    Update colors/linestyles for display/print.

--- mwhutils/plotting.py
+++ mwhutils/plotting.py
@@ -7,9 +7,59 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import numpy as np
+import itertools as it
 import matplotlib.pyplot as pl
 
-__all__ = ['figure', 'plot_pairs', 'show']
+__all__ = ['figure', 'plot_pairs', 'set_context', 'show']
+
+
+def set_context(context):
+    """
+    Set the parameters for plotting according to the given context.
+    """
+    global PARAMS
+    if context is 'screen':
+        PARAMS = dict(
+            colors=[
+                (0.21568627450980393, 0.49411764705882355, 0.7215686274509804),
+                (0.8941176470588236, 0.10196078431372549, 0.10980392156862745),
+                (0.30196078431372547, 0.6862745098039216, 0.2901960784313726),
+                (0.596078431372549, 0.3058823529411765, 0.6392156862745098),
+                (1.0, 0.4980392156862745, 0.0),
+                (0.6509803921568628, 0.33725490196078434, 0.1568627450980392),
+                (0.9686274509803922, 0.5058823529411764, 0.7490196078431373)],
+            dashes=[()],
+            alphab=0.25,
+            alpham=0.2,
+            weight=2,
+            markers=['o', 'x', 's', '+', 'D', '^'])
+    elif context is 'paper':
+        PARAMS = dict(
+            colors=[
+                (0.21568627450980393, 0.49411764705882355, 0.7215686274509804),
+                (0.8941176470588236, 0.10196078431372549, 0.10980392156862745),
+                (0.30196078431372547, 0.6862745098039216, 0.2901960784313726),
+                (0.596078431372549, 0.3058823529411765, 0.6392156862745098),
+                (1.0, 0.4980392156862745, 0.0),
+                (0.6509803921568628, 0.33725490196078434, 0.1568627450980392),
+                (0.9686274509803922, 0.5058823529411764, 0.7490196078431373)],
+            dashes=[
+                (),
+                (8, 4),
+                (6, 3, 2, 3),
+                (2, 2),
+                (6, 3, 2, 3, 2, 3),
+                (4, 4)],
+            alphab=0.25,
+            alpham=0.2,
+            weight=2,
+            markers=['o', 'x', 's', '+', 'D', '^'])
+    else:
+        raise ValueError('unknown context')
+
+
+# set the default context
+set_context('screen')
 
 
 class Axis(object):
@@ -18,14 +68,23 @@ class Axis(object):
         self._lim = (None, None, None, None)
         self._hook = (lambda: None) if (hook is None) else hook
         self._hold = False
+        self._init()
+
+    def _init(self):
+        self._dashes = it.cycle(PARAMS['dashes'])
+        self._markers = it.cycle(PARAMS['markers'])
+        self._lcolors = it.cycle(PARAMS['colors'])
+        self._mcolors = it.cycle(PARAMS['colors'])
 
         # pre-figure business
+        self._ax.cla()
         self._ax.spines['top'].set_visible(False)
         self._ax.spines['right'].set_visible(False)
-        self._ax.get_xaxis().tick_bottom()
-        self._ax.get_yaxis().tick_left()
+        self._ax.xaxis.set_tick_params(direction='out', top=False)
+        self._ax.yaxis.set_tick_params(direction='out', right=False)
+        self._ax.grid(color='k', ls='-', alpha=0.2, lw=0.5)
 
-    ### AXIS PROPERTIES =======================================================
+    # AXIS PROPERTIES =========================================================
 
     @property
     def title(self):
@@ -57,7 +116,7 @@ class Axis(object):
         self._ax.set_ylabel(ylabel)
         self._draw()
 
-    ### UTILITY METHODS =======================================================
+    # UTILITY METHODS =========================================================
 
     def set_lim(self, xmin=None, xmax=None, ymin=None, ymax=None):
         """
@@ -76,66 +135,67 @@ class Axis(object):
             self._ax.set_yticklabels([])
         self._draw()
 
-    ### PLOTTING METHODS ======================================================
+    # PLOTTING METHODS ========================================================
 
-    def scatter(self, x, y, marker='o', color='k', alpha=1):
+    def scatter(self, x, y, label=''):
         """
         Add a scatter plot to the axis.
         """
-        kwargs = dict(lw=1.5, marker=marker, color=color, alpha=alpha)
-        if marker is 'o':
-            kwargs['s'] = 30
-            kwargs['facecolors'] = 'none'
-        else:
-            kwargs['s'] = 40
-
-        self._ax.scatter(x, y, **kwargs)
+        marker = next(self._markers)
+        self._ax.scatter(x, y, lw=1.5, marker=marker, color='k',
+                         s=40, facecolors='none', label=label)
         self._draw()
 
-    def plot(self, x, y=None, color=None, alpha=1):
+    def scatterc(self, x, y, label=''):
+        color = next(self._mcolors)
+        self._ax.scatter(x, y, label=label, alpha=PARAMS['alpham'], color='k',
+                         facecolor=color, lw=0.1)
+        self._draw()
+
+    def plot(self, x, y=None, lo=None, hi=None, add=True, label=''):
         """
         Add a simple plot to the axis.
         """
         if y is None:
             y = x
             x = np.arange(len(y))
-        self._ax.plot(x, y, lw=2, color=color, alpha=alpha)
+
+        color = next(self._lcolors)
+        dashes = next(self._dashes)
+        self._ax.plot(x, y, ls='-', color=color, dashes=dashes, label=label,
+                      lw=PARAMS['weight'])
+
+        if (lo is not None) or (hi is not None):
+            hi = ((y+hi) if add else hi) if (hi is not None) else (y+lo)
+            lo = ((y-lo) if add else lo) if (lo is not None) else y
+            self._ax.fill_between(x, lo, hi, color=color, 
+                                  alpha=PARAMS['alphab'])
+
         self._draw()
 
-    def plot_banded(self, x, y, a=None, b=None, label=''):
-        if a is None and b is None:
-            lo = np.zeros_like(y)
-            hi = y
-        elif b is None:
-            lo = y - a
-            hi = y + a
-        else:
-            lo = y - a
-            hi = y + b
-        lines = self._ax.plot(x, y, lw=2, label=label)
-        color = lines[0].get_color()
-        alpha = 0.25
-        self._ax.fill_between(x, lo, hi, color=color, alpha=alpha)
-        self._draw()
-
-    def vline(self, x):
+    def vline(self, x, label=''):
         """
         Add a vertical line to the axis at x.
         """
-        self._ax.axvline(x, ls='--', color='r')
+        color = next(self._lcolors)
+        self._ax.axvline(x, ls='--', lw=2, color=color, label=label)
         self._draw()
 
-    def hline(self, y):
+    def hline(self, y, label=''):
         """
         Add a horizontal line to the axis at y.
         """
-        self._ax.axhline(y, ls='--', color='r')
+        color = next(self._lcolors)
+        self._ax.axhline(y, ls='--', lw=2, color=color, label=label)
         self._draw()
 
     def _draw(self):
         self._ax.axis('tight')
         self._ax.axis(self._lim)
-        self._ax.legend(loc='best')
+        handles, labels = self._ax.get_legend_handles_labels()
+        if len(handles) > 0:
+            self._ax.legend(handles, labels, frameon=False, loc='best',
+                            fontsize=12)
         self._hook()
         if not self._hold and pl.isinteractive():
             self.draw()
@@ -144,7 +204,7 @@ class Axis(object):
         self._hold = hold
 
     def clear(self):
-        self._ax.cla()
+        self._init()
         self._draw()
 
     def draw(self):
@@ -226,7 +286,7 @@ class Figure(object):
         self._fig.show(warn=False)
 
 
-def figure(num=None, rows=1, cols=1, figsize=None):
+def figure(rows=1, cols=1, num=None, figsize=None):
     toolbar = pl.rcParams['toolbar']
     pl.rcParams['toolbar'] = 'None'
     fig = pl.figure(num, facecolor='white', figsize=figsize)
@@ -240,17 +300,17 @@ def figure(num=None, rows=1, cols=1, figsize=None):
         return Figure(fig, rows, cols)
 
 
-def plot_pairs(samples, names=None, fig=None):
+def plot_pairs(samples, names=None, num=None):
     samples = np.array(samples, ndmin=2)
     _, d = samples.shape
     names = ['' for _ in xrange(d)] if (names is None) else names
 
-    fig = figure(fig, d-1, d-1)
+    fig = figure(d-1, d-1, num=num)
     fig.hold()
 
     for i in xrange(d):
         for j in xrange(i+1, d):
-            fig[j-1, i].scatter(samples[:, i], samples[:, j], alpha=0.1)
+            fig[j-1, i].scatterc(samples[:, i], samples[:, j])
             if i == 0:
                 fig[j-1, i].ylabel = names[j]
             if j == d-1:
